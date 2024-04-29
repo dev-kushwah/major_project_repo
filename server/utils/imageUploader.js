dfe6a705a3c2e799276e8ff7d69c45ede84ee4f8
Saksham agrawal <saksham.ag10262002sa@gmail.com>
2024-04-29 12:58:57 +0530

    change

--- server/utils/imageUploader.js
+++ server/utils/imageUploader.js
@@ -5,10 +5,9 @@ exports.uploadImageToCloudinary  = async (file, folder, height, quality) => {
         options.height = height;
     }
     if(quality) {
-        options.quality = quality;
+        options.quality = quality; 
     }
     options.resource_type = "auto";
-    
 
     return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
