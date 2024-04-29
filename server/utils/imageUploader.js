96ae2081a031f852a3a1fdce32c0f45f783c4fa7
Saksham agrawal <saksham.ag10262002sa@gmail.com>
2024-04-29 12:49:36 +0530

    image change route

--- server/utils/imageUploader.js
+++ server/utils/imageUploader.js
@@ -1,6 +1,4 @@
 const cloudinary = require('cloudinary').v2
-
-
 exports.uploadImageToCloudinary  = async (file, folder, height, quality) => {
     const options = {folder};
     if(height) {
@@ -10,6 +8,7 @@ exports.uploadImageToCloudinary  = async (file, folder, height, quality) => {
         options.quality = quality;
     }
     options.resource_type = "auto";
+    
 
     return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
